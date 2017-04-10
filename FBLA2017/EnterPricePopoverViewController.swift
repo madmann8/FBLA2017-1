dc6b6547131848f657707dc4683a578ab1989d20
lukemann <bs@lukejmann.com>
2017-04-09 23:34:43 -0400

    Weird Text Formatting

--- FBLA2017/EnterPricePopoverViewController.swift
+++ FBLA2017/EnterPricePopoverViewController.swift
@@ -6,13 +6,31 @@
 //  Copyright © 2017 Luke Mann. All rights reserved.
 //
 
+
+
+
 import UIKit
+
+
+
+
+protocol EnterPriceDelegate {
+    func retrievePrice(price: Int)
+}
 
 class EnterPricePopoverViewController: UIViewController {
 
+    var amount: Int { return moneyTextBox.string.digits.integer }
+
+    @IBOutlet weak var moneyTextBox: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor=UIColor.blue
+        moneyTextBox.delegate=self
+        moneyTextBox.textAlignment = .right
+        moneyTextBox.keyboardType = .numberPad
+        moneyTextBox.text=Formatter.decimal.string(from: amount as NSNumber)
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,6 +39,7 @@ class EnterPricePopoverViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    var delegate:EnterPriceDelegate? = nil
 
     /*
     // MARK: - Navigation
@@ -32,4 +51,39 @@ class EnterPricePopoverViewController: UIViewController {
     }
     */
 
+}
+
+extension EnterPricePopoverViewController: UITextViewDelegate{
+    func textViewDidChange(_ textView: UITextView) {
+        textView.text=Formatter.decimal.string(from: amount as NSNumber)
+    }
+}
+
+
+
+extension UITextView {
+    var string: String { return text ?? "" }
+}
+
+extension NumberFormatter {
+    convenience init(numberStyle: NumberFormatter.Style) {
+        self.init()
+        self.numberStyle = numberStyle
+    }
+}
+struct Formatter {
+    static let decimal = NumberFormatter(numberStyle: .decimal)
+}
+
+
+extension String {
+    private static var digitsPattern = UnicodeScalar("0")..."9"
+    var digits: String {
+        return unicodeScalars.filter { String.digitsPattern ~= $0 }.string
+    }
+    var integer: Int { return Int(self) ?? 0 }
+}
+
+extension Sequence where Iterator.Element == UnicodeScalar {
+    var string: String { return String(String.UnicodeScalarView(self)) }
 }
