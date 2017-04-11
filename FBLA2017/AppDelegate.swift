752e186759a2fccf48cc08d94557ee7aaac8b5e4
lukemann <bs@lukejmann.com>
2017-04-11 02:01:22 -0400

    Actually End Day 3: Changed GoogleService-Info name

--- FBLA2017/AppDelegate.swift
+++ FBLA2017/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 
 @UIApplicationMain
@@ -22,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         window?.tintColor=themeColor
         
-
+      FIRApp.configure()
         
         
         return true
