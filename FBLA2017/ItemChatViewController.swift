c38a6bfbd7c27f855157053b28b01d92b3804b5b
lukemann <bs@lukejmann.com>
2017-04-14 00:07:57 -0400

    Chat VC is ready to work on and is connected to the SB

---
+++ FBLA2017/ItemChatViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ItemChatViewController.swift
+//  FBLA2017
+//
+//  Created by Luke Mann on 4/14/17.
+//  Copyright © 2017 Luke Mann. All rights reserved.
+//
+
+import UIKit
+import ChameleonFramework
+
+class ItemChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        self.view.backgroundColor=UIColor.flatLime
+    }
+}
