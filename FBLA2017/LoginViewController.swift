08eb29aa36119ddaea4dc47c500e450f11810861
lukemann <bs@lukejmann.com>
2017-04-14 23:39:00 -0400

    About to add Facebook SDK

--- FBLA2017/LoginViewController.swift
+++ FBLA2017/LoginViewController.swift
@@ -2,34 +2,14 @@
 //  LoginViewController.swift
 //  FBLA2017
 //
-//  Created by Luke Mann on 4/11/17.
+//  Created by Luke Mann on 4/14/17.
 //  Copyright © 2017 Luke Mann. All rights reserved.
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
