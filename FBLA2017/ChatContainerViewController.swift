606d65bea2b6b3ebaf2d54523eff93de0f26b5a2
lukemann <bs@lukejmann.com>
2017-04-18 23:59:24 -0400

    Set up stuff for making two chats together

---
+++ FBLA2017/ChatContainerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChatContainerViewController.swift
+//  FBLA2017
+//
+//  Created by Luke Mann on 4/18/17.
+//  Copyright © 2017 Luke Mann. All rights reserved.
+//
+
+import UIKit
+
+class ChatContainerViewController: UIViewController {
+
+    @IBOutlet weak var dmChatContainer: UIView!
+    @IBOutlet weak var globalChat: UIView!
+    
+    @IBAction func switchChanged(_ sender: UISegmentedControl) {
+    }
+}
